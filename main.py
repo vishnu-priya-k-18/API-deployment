5f58645a4cdc734af0c81f2ce9aef599d6eda83e
vishnupriya.k <vishnupriya.k@thoughtworks.com>
2021-11-04 20:36:02 +0530

    refactor code

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request, jsonify
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from flask import Flask, request
 import pickle
 import numpy as np
-
 import requests
-app = Flask(__name__,template_folder='templates')
+
+app = Flask(__name__, template_folder='templates')
+
 model = pickle.load(open('model.pkl', 'rb'))  # load
-@app.route('/',methods = ['GET','POST'])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def getResult():
     if request.method == "GET":
         url = "http://127.0.0.1:5000/"
@@ -18,17 +19,17 @@ def getResult():
         }
 
         r = requests.post(url, json=data)
+
         return str(r.json())
 
     if request.method == "POST":
-
-
         data = request.get_json()
-        experienceOfEmployee = data["experience"]
-        user_input = np.array([[experienceOfEmployee]])
+        experienceOfEmployees = data["experience"]
+        user_input = np.array([[experienceOfEmployees]])
         prediction = model.predict(user_input)
 
         return str(prediction)
+
 
 if __name__ == "__main__":
     app.debug = True
--- model.py
+++ model.py
@@ -3,15 +3,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pickle
 
-dataset = pd.read_csv('Salary.csv') # read excel
-X = dataset.iloc[:, :-1].values # input
-y = dataset.iloc[:, 1].values # values get predicted
+dataset = pd.read_csv('Salary.csv')  # read excel
+X = dataset.iloc[:, :-1].values  # input
+y = dataset.iloc[:, 1].values  # values get predicted
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3, random_state=0)
 
 regression = LinearRegression()
 regression.fit(X_train, y_train)
 
-y_pred = regression.predict(X_test)
+yPrediction = regression.predict(X_test)
 
 pickle.dump(regression, open('model.pkl', 'wb'))
